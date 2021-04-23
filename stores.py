2047a2a0ffd904b62e0d4533737d6fedb61c19f8
Boyd <boydpires@gmail.com>
2021-04-23 21:44:18 +0530

    Initial commit with non persistent data

---
+++ stores.py
@@ -0,0 +1,36 @@
+stores = [
+    {
+        'name': 'store1',
+        'items': [
+            {
+                'name': 'item1',
+                'price': 999 
+            },
+            {
+                'name': 'item2',
+                'price': 999 
+            }
+        ]
+    },
+    {
+        'name': 'store2',
+        'items': [
+            {
+                'name': 'item1',
+                'price': 999 
+            },
+            {
+                'name': 'item2',
+                'price': 999 
+            },
+            {
+                'name': 'item3',
+                'price': 999 
+            }
+        ]
+    },
+    {
+        'name': 'store3',
+        'items': []
+    }
+] 
